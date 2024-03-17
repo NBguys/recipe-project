e27d9aafd1fc5334c44922c068df6fc47dba9c5a
NBguys <gudwns520@gamil.com>
2024-03-17 14:21:54 +0900

    드롭다운 리스트 기능 동작 버그를 디렉티브로 표시 가능하도록 수정

---
+++ src/app/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Renderer2, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  @HostBinding('class.show') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+    let part = this.el.nativeElement.querySelector(".dropdown-menu");
+    if(this.isOpen) this.renderer.addClass(part, 'show');
+    else this.renderer.removeClass(part, 'show');
+  }
+
+}
