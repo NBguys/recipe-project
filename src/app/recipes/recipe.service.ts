3916daca10edd8062e33e9f83486eb553f968dec
NBguys <gudwns520@gamil.com>
2024-03-17 14:20:30 +0900

    레시피 서비스 추가 쇼핑 리스트의 재료를 레시피에 추가 하는 기능 추가

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,41 @@
-import { EventEmitter } from '@angular/core'
+import { EventEmitter, Injectable } from '@angular/core'
 
 import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model'
+import { ShoppingListService } from '../shopping-list/shopping-list.service'
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>;
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe a', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Recipe b', 'Easy recipes', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
+        new Recipe('Recipe a', 
+        'This is simply a test', 
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingredient('Meat',1),
+          new Ingredient('Fires',20)
+        ]),
+        new Recipe('Recipe b', 
+        'Easy recipes', 
+        'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+        [
+          new Ingredient('Buns',2),
+          new Ingredient('Meat',1)
+        ])
       ];
+
+      constructor(private slService: ShoppingListService) {
+
+      }
 
       getRecipes() {
         return this.recipes.slice();
+      }
+
+      addIngredientToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService ]
+  providers: [ RecipeService ]
 })
 export class RecipesComponent implements OnInit {
 
